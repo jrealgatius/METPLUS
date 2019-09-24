fcb62fafbe1039ab4ee72785491f870214a1759a
jrealgatius <jordireal@gmail.com>
2019-09-24 13:44:11 +0200

    Netejat fitxer de prova de gaps

--- codi/funcio_stops_tractaments.R
+++ codi/funcio_stops_tractaments.R
@@ -4,14 +4,12 @@
 
 ## la data index i finestra serveix per filtrar
 
-dades_test<-tibble::tibble(idp=c(1,1,1,2,2,3,3),data=c(1,10,5,3,15,9,15),datafi=c(3,15,25,4,20,10,20))
-dades_test<-tibble::tibble(idp=c(1,1,1,2,2,3,3),data=c(1,10,5,2,15,9,15),datafi=c(3,15,25,14,20,10,20))
 
-dades_test<-tibble::tibble(idp=c(1,1,1,2,2,3,3),
-                           data=c("20190101","20190110","20190105","20190102","20190115","20190109","20190115"),
-                           datafi=c("20190103","20190115","20190125","20190114","20190120","20190110","20190120"))
 
-dades_test<-dades_test %>% mutate(data2=as.Date(data,format="%Y%m%d"),datafi2=as.Date(datafi,format="%Y%m%d"))
+# Historic de farmacs: idp, datinici,datafi, gap
+# Elimina solapaments i discontinuitats petites i retorna dades sense discontinuitats ni solapaments amb igual o menys registres
+# Retorna dades amb : id, datainici i datafi amb menys registres, havent eliminat solapaments i gaps (discontinuitat petita)
+
 
 
 # Historic de farmacs: idp, datinici,datafi, gap
@@ -20,44 +18,59 @@ dades_test<-dades_test %>% mutate(data2=as.Date(data,format="%Y%m%d"),datafi2=as
 
 agregar_solapaments_gaps<-function(dt=dades,id="idp",datainici="data",datafinal="datafi",gap=5){
   
-  # dt=dades_test
-  # gap=10
-  # datainici="data2"
-  # datafinal="datafi2"
+  # dt=FX.FACTURATS_PRESCRITS_GRUPS
+  # gap=60
+  # datainici="dat"
+  # datafinal="datafi"
   # id="idp"
-
+  
   # Conversió a Sym per evaluació  
   datainici_sym<-rlang::sym(datainici)
   datafinal_sym<-rlang::sym(datafinal)
   idp_sym=rlang::sym(id)
-    
+  
   # Seleccionar dades necessaries amb noms sense sym
   dt<-dt %>% select(idp=!!idp_sym, data=!!datainici_sym,datafi=!!datafinal_sym)
   
+  
+  # 1. Eliminar solapaments 
+  dt<-dt %>% 
+    group_by(idp) %>% 
+    arrange(data) %>% 
+    mutate(indx = c(0, cumsum(as.numeric(lead(data)) >
+                                cummax(as.numeric(datafi)))[-n()])) %>%
+    group_by(idp, indx) %>%
+    summarise(data = min(data), datafi = max(datafi)) %>%
+    select(-indx) 
+  
+  
+  # 2. ELiminar Gaps (discontinuitats)
+  
   # MAP_ggplot(dades=dt,datainicial="data",datafinal="datafi",id="idp",grup_color=NA,grup_linea=NA)
   dt<-dt %>% 
-    arrange(idp,data,datafi) %>% 
-    group_by(idp) %>% mutate(gap1=(data-lag(datafi))) %>% 
-    mutate(gap2=case_when(gap1 > gap ~1, TRUE ~0)) %>% 
-    group_by(idp) %>% 
-    mutate(gap3=(cumsum(gap2)))%>%ungroup()
+    mutate(gap1=(data-lag(datafi))) %>%
+    mutate(gap2=case_when(gap1 > gap ~1, TRUE ~0)) %>%
+    mutate(gap3=(cumsum(gap2))) %>% 
+    ungroup()
   
-  # Agregate 
-  dt2<-dt %>%  mutate (idp2=idp) %>% 
+  # 3. Agregate per idp-gap
+  dt2<-dt %>%  mutate (idp2=idp) %>%
     select(idp,data,datafi,gap3,idp,idp2) %>%
     group_by(idp,gap3)%>%
-    summarise(datainici= min(data), datafi= max(datafi),idp2=min(idp2)) %>% 
-    ungroup() 
+    summarise(datainici= min(data), datafi= max(datafi),idp2=min(idp2)) %>%
+    ungroup()
   
-  # MAP_ggplot(dades=dt2,datainicial="data",datafinal="datafi",id="idp")
-  dt2<-dt2 %>% select("idp","datainici","datafi") 
+  dt2<-dt2 %>% select("idp","datainici","datafi")
+  
+  # MAP_ggplot(dades= dt2 %>% head(10),datainicial="datainici",datafinal="datafi",id=id)
   
   # Renombro noms dels camps originals
   colnames(dt2)<-c(idp_sym,datainici_sym,datafinal_sym)
-
+  
   dt2
   
 }
+
 
 
 
@@ -72,82 +85,6 @@ dades_test<-dades_test %>% mutate(data2=as.Date(data,format="%Y%m%d"),datafi2=as
 MAP_ggplot_univariant(dades_test,datainicial = "data2",datafinal = "datafi2",id="idp",Nmostra = Inf)
 dades_agr<-agregar_solapaments_gaps(dt=dades_test,id="idp",datainici = "data2",datafinal="datafi2",gap=30)
 MAP_ggplot_univariant(dades_agr,datainicial = "data2",datafinal = "datafi2",id="idp",Nmostra = Inf)
-
-
-
-
-
-# Filtrar per registres que inicien prescripció fora de la finestra (24 mesos)
-# FX.FACTURATS_PRESCRITS_GRUPS<-FX.FACTURATS_PRESCRITS_GRUPS %>% filter(dat<=dtindex+finestra)
-
-FX.FACTURATS_PRESCRITS_GRUPS<-FX.FACTURATS_PRESCRITS_GRUPS %>% 
-  filter(idp=="00a1c12dce4f3e85c317c84bfd0f858ea7d3c68a" & GRUP=="IDPP4") 
-
-gap_dies<-35
-farmacs_dt_sense_gaps<-agregar_solapaments_gaps(FX.FACTURATS_PRESCRITS_GRUPS,id="idp",datainici = "dat",datafinal="datafi",gap=gap_dies)
-
-MAP_ggplot_univariant(FX.FACTURATS_PRESCRITS_GRUPS %>% filter(GRUP=="IDPP4"),datainicial = "dat",datafinal = "datafi",id="idp",Nmostra = 1)
-MAP_ggplot_univariant(farmacs_dt_sense_gaps ,datainicial = "dat",datafinal = "datafi",id="idp",Nmostra = 1)
-
-
-### Ojo que n'hi ha que tenen dat i datafi la mateixa data (S'haurien d'eliminar)
-
-
-# analisis_OT<-T
-
-if (analisis_OT==T) {
-  
-  dt_data_stop<-select(dt_grups,idp,grup) %>% 
-    left_join(dt_farmacs_FD_STOP %>% 
-                full_join(select(dt_fx.canvis,-dtindex),by="idp"),by="idp")%>% 
-    mutate(datafiOT= case_when(
-      grup=="IDPP4" ~ pmin(STOP.FD.IDPP4,CANVITX.iSGLT2,CANVITX.SU,na.rm=T),
-      grup=="ISGLT2" ~ pmin(STOP.FD.iSGLT2,CANVITX.IDPP4,CANVITX.SU,na.rm=T),
-      grup=="SU" ~ pmin(STOP.FD.SU,CANVITX.iSGLT2,CANVITX.IDPP4,na.rm=T)),
-      datafiOT=ifelse(is.na(datafiOT),dtindex,datafiOT)) %>% 
-    mutate (datafiOT=lubridate::as_date(datafiOT)) %>% select(idp,grup,dtindex,datafiOT)
-} else {
-  dt_data_stop<-select(dt_grups,idp,grup) %>% 
-    mutate(datafiOT=lubridate::ymd(20181231)) %>% 
-    select(idp,datafiOT)
-}
-
-dt_data_stop %>% filter(datafiOT<dtindex)
-
-dt_grups %>% filter(idp=="019e464329f11f97ac5eab29d1cd0f1f62fa34b3")
-dt_farmacs_FD_STOP %>% filter(idp=="019e464329f11f97ac5eab29d1cd0f1f62fa34b3")
-dt_farmacs_STOP %>% filter(idp=="019e464329f11f97ac5eab29d1cd0f1f62fa34b3")
-
-FX.FACTURATS_GRUPS %>% filter(idp=="019e464329f11f97ac5eab29d1cd0f1f62fa34b3")
-FX.PRESCRITS_GRUPS %>% filter(idp=="019e464329f11f97ac5eab29d1cd0f1f62fa34b3")
-FX.FACTURATS_PRESCRITS_GRUPS %>% filter(idp=="019e464329f11f97ac5eab29d1cd0f1f62fa34b3")
-farmacs_dt_sense_gaps %>% filter(idp=="019e464329f11f97ac5eab29d1cd0f1f62fa34b3")
-
-
-dt_data_stop %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-dt_grups %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-dt_farmacs_FD_STOP %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-dt_farmacs_STOP %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-
-FX.FACTURATS_GRUPS %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-FX.PRESCRITS_GRUPS %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-FX.FACTURATS_PRESCRITS_GRUPS %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-farmacs_dt_sense_gaps %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-farmacs_grups %>% filter(idp=="04ea33a577abb26ac5fa344406df3759629bb403")
-
-
-dt_grups %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-dt_farmacs_FD_STOP %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-dt_farmacs_STOP %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-
-FX.FACTURATS_GRUPS %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-FX.PRESCRITS_GRUPS %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-FX.FACTURATS_PRESCRITS_GRUPS %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-farmacs_dt_sense_gaps %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-farmacs_grups %>% filter(idp=="0b177a539555873cde1b7621890cb7085b6bc0a2")
-
-FX.PRESCRITS_GRUPS %>% filter(dat==dbaixa) 
-FX.FACTURATS_PRESCRITS_GRUPS %>% filter(env==0) 
 
 
 
